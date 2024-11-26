ced081e8fe281fd173c1529b6663685444770f62
sharmababita <sharma.089babita@gmail.com>
2024-11-26 12:02:33 +0100

    oppgave2... Error

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Hello!');
+const submitButton = document.querySelector("#submitButton");
+
+submitButton.addEventListener("click", function() {
+    event.preventDefault();
+    console.log("Button is clicked!")
+});
